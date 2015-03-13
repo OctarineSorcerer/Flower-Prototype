8da726f1e03c9673bab938e4e8f2f1a7da3bc479
OctarineSorcerer <gamer633697@gmail.com>
2015-03-13 21:25:49 +0000

    Start of main menu changes a bit

--- core/src/com/mygdx/game/MainMenuScreen.java
+++ core/src/com/mygdx/game/MainMenuScreen.java
@@ -1,16 +1,22 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.Application;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+
+import java.util.*;
+import java.util.List;
 
 public class MainMenuScreen implements Screen {
     private Stage stage = new Stage();
@@ -20,6 +26,7 @@ public class MainMenuScreen implements Screen {
             new TextureAtlas(Gdx.files.internal("skins/menuSkin.pack")));
 
     private TextButton buttonPlay = new TextButton("Play", skin),
+            buttonFlower = new TextButton("Flower", skin),
             buttonExit = new TextButton("Exit", skin);
     private Label titleLabel = new Label("Flower Prototype!", skin, "MotorStyle");
 
@@ -30,8 +37,42 @@ public class MainMenuScreen implements Screen {
     public MainMenuScreen(final FlowerPrototype gam) { //no create() here, so constructor is used
         game = gam;
 
+        buttonPlay.addListener(new ClickListener()
+        {
+            @Override
+            public void clicked(InputEvent event, float x, float y)
+            {
+                ((Game)Gdx.app.getApplicationListener()).setScreen(new GameScreen(game));
+            }
+        });
+        buttonExit.addListener(new ClickListener()
+        {
+            @Override
+            public void clicked(InputEvent event, float x, float y)
+            {
+                Gdx.app.exit();
+            }
+        });
+
         camera = new OrthographicCamera();
         camera.setToOrtho(false, FlowerPrototype.WIDTH, FlowerPrototype.HEIGHT); //Ah, orthographic views. All hail 2d.
+    }
+
+    private ArrayList<Texture> GetMonochromes(String path) {
+        ArrayList<Texture> output = new ArrayList<Texture>();
+        FileHandle dirHandle;
+        if (Gdx.app.getType() == Application.ApplicationType.Android) {
+            dirHandle = Gdx.files.internal(path);
+        } else {
+            // ApplicationType.Desktop ..
+            dirHandle = Gdx.files.internal("./bin/" + path);
+        }
+        for (FileHandle entry: dirHandle.list()) {
+            if (entry.name().endsWith(".png")) {
+                output.add(new Texture(entry));
+            }
+        }
+        return output;
     }
 
     @Override
@@ -60,22 +101,7 @@ public class MainMenuScreen implements Screen {
 
     private void PlayExitOptions() {
         stage.clear();
-        buttonPlay.addListener(new ClickListener()
-        {
-            @Override
-            public void clicked(InputEvent event, float x, float y)
-            {
-                ((Game)Gdx.app.getApplicationListener()).setScreen(new GameScreen(game));
-            }
-        });
-        buttonExit.addListener(new ClickListener()
-        {
-            @Override
-            public void clicked(InputEvent event, float x, float y)
-            {
-                Gdx.app.exit();
-            }
-        });
+        table.clear();
 
         //For table: The elements are displayed in the order added.
         //The first appear on top, the last at the bottom.
@@ -88,7 +114,14 @@ public class MainMenuScreen implements Screen {
     }
 
     private void NewFlowerCreation() {
-        Image actor = new Image();
+        stage.clear();
+        table.clear();
+
+        ArrayList<Texture> petals = GetMonochromes("textures/petals/monochrome");
+        for(Texture petalTex : petals) {
+            Image petalImage = new Image(petalTex);
+            table.row();
+        }
     }
 
     @Override
