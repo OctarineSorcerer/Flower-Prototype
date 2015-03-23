4260c61a5db3a533eb51f39a1d1e111f2e42abcf
Dan <gamer633697@gmail.com>
2015-03-23 01:29:35 +0000

    Old screen, vertical scrolling
    Yay for vertical scrolling! Need that for many petals

--- core/src/com/mygdx/game/MainMenuScreen.java
+++ core/src/com/mygdx/game/MainMenuScreen.java
@@ -18,24 +18,14 @@ import com.sun.javafx.geom.Point2D;
 import java.util.*;
 
 public class MainMenuScreen implements Screen {
-    private Skin skin = new Skin(Gdx.files.internal("skins/uiskin.json"));
-    private class PetalSlider {
-        public int imageIndex;
-        public Slider slider = new Slider(0, 10, 1, false, skin);
-        public PetalSlider(int imageIndex) {
-            this.imageIndex = imageIndex;
-            slider.setValue(3);
-            sliders.add(this);
-        }
-    }
-
     private Stage stage = new Stage();
     private Table table = new Table();
 
+    private Skin skin = new Skin(Gdx.files.internal("skins/uiskin.json"));
+
     private TextButton buttonFlower = new TextButton("Flowers", skin),
             buttonExit = new TextButton("Exit", skin);
-    private ArrayList<PetalSlider> sliders = new ArrayList<PetalSlider>();
-    private ArrayList<Image> petalImages = new ArrayList<Image>();
+    private Slider slider = new Slider(3, 20, 1, false, skin);
     private Label titleLabel = new Label("Flower Prototype!", skin);
 
     Color[] permittedColours = new Color[] {
@@ -44,9 +34,7 @@ public class MainMenuScreen implements Screen {
             Color.TEAL, Color.WHITE, Color.YELLOW
     };
 
-    Map<Integer, ArrayList<Color>> petals = new HashMap<Integer, ArrayList<Color>>();
-    ArrayList<Integer> petalIndices = new ArrayList<Integer>();
-
+    Map<String, ArrayList<Color>> petals = new HashMap<String, ArrayList<Color>>();
     Image headImage, headColourImage; //this is so only 1 can be selected
     Color headColour, stemColour;
     String stemThickness;
@@ -110,22 +98,6 @@ public class MainMenuScreen implements Screen {
         return images;
     }
 
-    /**
-     * Gets the index of the first positive slider after (and including) the index given
-     * @param index
-     * @return
-     */
-    public int GetChosenSliderAfter(int index) {
-        int firstChosen = -1;
-        for(int i = index; i < sliders.size() && firstChosen == -1; i++) {
-            PetalSlider petalSlider = sliders.get(i);
-            if(petalSlider.slider.getValue() > 0) {
-                firstChosen = i;
-            }
-        }
-        return firstChosen;
-    }
-
     @Override
     public void render(float delta) {
         //Gdx.gl.glClearColor(0.1059f, 0.1059f, 0.1059f, 1); //kinda a dark grey color
@@ -186,7 +158,7 @@ public class MainMenuScreen implements Screen {
             @Override
             public void clicked(InputEvent event, float x, float y)
             {
-                ChoosePetalSprites();
+                ChoosePetalColours();
             }
         });
         subTable.add(createButton).fillX().center().row();
@@ -197,103 +169,72 @@ public class MainMenuScreen implements Screen {
         stage.addActor(table);
     }
 
-    public void ChoosePetalSprites() {
+    public void ChoosePetalColours() {
         stage.clear();
         table.clear();
-        table.add(new Label("Pick your petals and colours!", skin)).colspan(2).pad(10).fillX().center().row();
+        table.add(new Label("Pick your petals and colours!", skin)).pad(10).fillX().center().row();
+        Table subTable = new Table();
 
-        Table scrollTable = new Table();
-        petalImages = GetImages("textures/petals/monochrome");
-        for(int i = 0; i < petalImages.size(); i++) {
-            final Image image = petalImages.get(i);
-            //petals.put(i, new ArrayList<Color>());
-            scrollTable.add(image).pad(10).left().expandX().row();
-            final PetalSlider petalSlider = new PetalSlider(i);
-            final Label petalCount = new Label(Float.toString(petalSlider.slider.getValue()), skin);
-            petalSlider.slider.addListener(new com.badlogic.gdx.scenes.scene2d.utils.ChangeListener() {
-                @Override
-                public void changed(ChangeEvent event, Actor actor) {
-                    petalCount.setText(Float.toString(petalSlider.slider.getValue()));
-                }
-            });
-            petalSlider.slider.addListener(new InputListener() {
-                public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
-                    event.stop();
-                    return false;
-                }
-            });
-            //.addListener(stopTouchDown);
-            //sliders.add(petalSlider);
-            scrollTable.add(petalSlider.slider).fillX().pad(5);
-            scrollTable.add(petalCount).left().padRight(10).row();
+        ArrayList<Image> images = GetImages("textures/petals/monochrome");
+        for(final Image image : images) {
+            petals.put(image.getName(), new ArrayList<Color>());
+            Table colourTable = new Table();
+            colourTable.add(image).pad(10).center().fillX();
+            for(Color colour : permittedColours) {
+                final Image colourImage = new Image(new Texture("textures/ColourSquare.png"));
+                colourImage.setColor(colour.r, colour.g, colour.b, 0.5f);
+                colourImage.addListener(new ClickListener() {
+                    @Override
+                    public void clicked(InputEvent event, float x, float y)
+                    {
+                        //Add/remove colour to appropriate list
+                        Color colour = colourImage.getColor();
+                        String imageName = image.getName();
+
+                        if(petals.containsKey(imageName)) {
+                            ArrayList<Color> colourList = petals.get(imageName);
+                            if(colourList.contains(colour)) {
+                                colourList.remove(colour);
+                                colourImage.setColor(colour.r, colour.g, colour.b, 0.5f);
+                            }
+                            else {
+                                colourList.add(colour);
+                                colourImage.setColor(colour.r, colour.g, colour.b, 1.0f);
+                            }
+                        }
+                    }
+                });
+                colourTable.add(colourImage).padRight(10);
+            }
+            ScrollPane colourScroll = new ScrollPane(colourTable);
+            subTable.add(colourScroll).padBottom(5).row();
         }
         TextButton chooseHead = new TextButton("Next!", skin);
         chooseHead.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y)
             {
-                for(PetalSlider slider : sliders) {
-                    if(slider.slider.getValue() > 0) { //Ensures only chosen colours are in petalgroups
-                        petals.put(slider.imageIndex, new ArrayList<Color>());
-                    }
-                }
-                int firstChosen = GetChosenSliderAfter(0);
-                ChooseColours(firstChosen);
+                ChooseHead();
             }
         });
+        slider.setValue(10);
+        final Label progressLabel = new Label(Float.toString(slider.getValue()), skin);
+        slider.addListener(new com.badlogic.gdx.scenes.scene2d.utils.ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                progressLabel.setText(Float.toString(slider.getValue()));
+            }
+        });
+        ScrollPane wholePane = new ScrollPane(subTable);
+        wholePane.setScrollingDisabled(true, false);
+        wholePane.setCancelTouchFocus(false); //THIS combination of two lines was almost demonic to find
 
-        table.add(new ScrollPane(scrollTable)).colspan(2).fillX().center().row();
-        table.add(chooseHead).center();
+        table.add(wholePane).row();
+        table.add(slider).colspan(2).fillX().pad(5);
+        table.add(progressLabel).left().padRight(10).row();
+        table.add(chooseHead).colspan(2).center();
         table.setFillParent(true);
         stage.addActor(table);
-    }
-    public void ChooseColours(final int petalIndex) {
-        table.clear();
-
-        Image petalImage = petalImages.get(petalIndex);
-        table.add(petalImage).center().padBottom(20).row();
-
-        Table colourTable = new Table();
-        for(Color colour : permittedColours) {
-            final Image colourImage = new Image(new Texture("textures/ColourSquare.png"));
-            colourImage.setColor(colour.r, colour.g, colour.b, 0.5f);
-            colourImage.addListener(new ClickListener() {
-                @Override
-                public void clicked(InputEvent event, float x, float y)
-                {
-                    //Add/remove colour to appropriate list
-                    Color colour = colourImage.getColor();
-
-                    if(petals.containsKey(petalIndex)) {
-                        ArrayList<Color> colourList = petals.get(petalIndex);
-                        if(colourList.contains(colour)) {
-                            colourList.remove(colour);
-                            colourImage.setColor(colour.r, colour.g, colour.b, 0.5f);
-                        }
-                        else {
-                            colourList.add(colour);
-                            colourImage.setColor(colour.r, colour.g, colour.b, 1.0f);
-                        }
-                    }
-                }
-            });
-            colourTable.add(colourImage).padRight(10);
-        }
-        table.add(new ScrollPane(colourTable)).padBottom(5).row();
-        TextButton next = new TextButton("Next!", skin);
-        next.addListener(new ClickListener() {
-            @Override
-            public void clicked(InputEvent event, float x, float y)
-            {
-                if(petalIndex < petals.size() - 1) {
-                    ChooseColours(GetChosenSliderAfter(petalIndex + 1));
-                }
-                else {
-                    ChooseHead();
-                }
-            }
-        });
-        table.add(next).center();
     }
     public void ChooseHead() {
         stage.clear();
@@ -346,27 +287,18 @@ public class MainMenuScreen implements Screen {
             {
                 HeadSave head = new HeadSave(headColour, headImage.getName());
                 StemSave stem = new StemSave(new Random().nextLong(), Color.GREEN, 20,
-                new Point2D(FlowerPrototype.WIDTH/2, 20));
+                        new Point2D(FlowerPrototype.WIDTH/2, 20));
                 GrowthInfo growthInfo = new GrowthInfo(0, 6, 4);
                 ArrayList<PetalGroupSave> petalGroups = new ArrayList<PetalGroupSave>();
-                Random rand = new Random();
 
-                for (Integer key : petals.keySet())
+                for (String key : petals.keySet())
                 {
                     ArrayList<Color> colours = petals.get(key);
-                    ArrayList<Integer> groupIndices = new ArrayList<Integer>();
-                    //int colourAmount = colours.size();
                     for(Color colour : colours) {
-                        groupIndices.add(petalGroups.size() - 1);
-                        petalGroups.add(new PetalGroupSave(colour, petalImages.get(key).getName(), 1, 0));
-                    }
-                    int petalAmount = (int)(sliders.get(key).slider.getValue()); //Ye gods this is really cobbled together. Damn being ill
-                    for(int i = 0; i < petalAmount; i++) {
-                        petalIndices.add(groupIndices.get(rand.nextInt(groupIndices.size())));
+                        petalGroups.add(new PetalGroupSave(colour, key, 1, 0));
                     }
                 }
-                int petalCount = 0;
-
+                int petalCount = (int)(slider.getValue());
                 ArrayList<Integer> indices = Flower.GetIndexMix(petalGroups.size() - 1, petalCount);
                 SaveInfo info = new SaveInfo(head, stem, growthInfo, petalGroups, indices);
                 info.Name = "Test Save";
